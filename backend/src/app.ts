f67c164c94cb6fd9859fd68f0e3ce9ef584b8e13
aditi.joshi2022@vitstudent.ac.in <aditi.joshi2022@vitstudent.ac.in>
2024-09-27 20:56:20 +0530

    Second commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,16 +2,18 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 config();
 
 //middlewares
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove it in productions
 app.use(morgan("dev"));
 
-app.use("/api/v1", appRouter);
+app.use("/api/v1", appRouter);   //request made to this api endpt handled by approuter
 
 export default app;
